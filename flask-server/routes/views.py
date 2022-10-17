363385cacd8166482d39bab4d3edf47a98901ab8
Pedro <pbaggio765@gmail.com>
2022-10-17 17:44:53 -0400

    Add course search front end

--- flask-server/routes/views.py
+++ flask-server/routes/views.py
@@ -4,12 +4,24 @@ app_blueprint= Blueprint("app_blueprint", __name__)
 
 @app_blueprint.route('/')
 def home_view():
-    return render_template('base.html')
+    return render_template('index.html')
 
-@app_blueprint.route('/course/<mongoid>')
+@app_blueprint.route('/courses/<mongoid>')
 def course_view(mongoid):
-    doc = {"name": 'Course Name', 'professor': 'Professor Name', 'course_description': 'Doggo ipsum puggo wrinkler shoober big ol pupper, wow such tempt. Bork smol borking doggo with a long snoot for pats doggo wow very biscit much ruin diet, what a nice floof wrinkler. Ur givin me a spook very hand that feed shibe floofs pupper, bork. Floofs adorable doggo big ol you are doing me the shock, doggorino tungg. Vvv heck thicc many pats clouds puggorino boofers pupper, heckin good boys and girls heckin many pats doggorino many pats. You are doin me a concern doing me a frighten wow very biscit long woofer boof wow such tempt, smol wrinkler borking doggo extremely cuuuuuute. borkdrive shooberino pats. Big ol pupper fat boi snoot heck wow very biscit most angery pupper I have ever seen I am bekom fat, tungg very hand that feed shibe many pats shibe blep. Most angery pupper I have ever seen sub woofer h*ck, stop it fren.Doggo ipsum puggo wrinkler shoober big ol pupper, wow such tempt. Bork smol borking doggo with a long snoot for pats doggo wow very biscit much ruin diet, what a nice floof wrinkler. Ur givin me a spook very hand that feed shibe floofs pupper, bork. Floofs adorable doggo big ol you are doing me the shock, doggorino tungg. Vvv heck thicc many pats clouds puggorino boofers pupper, heckin good boys and girls heckin many pats doggorino many pats. You are doin me a concern doing me a frighten wow very biscit long woofer boof wow such tempt, smol wrinkler borking doggo extremely cuuuuuute. borkdrive shooberino pats. Big ol pupper fat boi snoot heck wow very biscit most angery pupper I have ever seen I am bekom fat, tungg very hand that feed shibe many pats shibe blep. Most angery pupper I have ever seen sub woofer h*ck, stop it fren.'}
+    doc = {"code": "CSCI-UA.0002", "section": "X", "date": {"days": ["T", "Th"], "start_time": "9:30AM", "end_time": "10:45AM"}, "name": 'Software Engineering', 'professor': 'X', 'description': 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.'}
     return render_template('course.html', mongoid=mongoid, doc=doc)
     
-
+@app_blueprint.route('/courses')
+def course_search_view():
+    docs = [
+            {"code": "CSCI-UA.0001", "section": "X", "date": {"days": ["T", "Th"], "start_time": "9:30AM", "end_time": "10:45AM"}, "name": 'Intro to Computer Science', 'professor': 'X', 'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam in finibus dolor. Ut ut sollicitudin ante. Praesent fringilla augue ante, vitae feugiat nisl consequat ut.'},
+            {"code": "CSCI-UA.0002", "section": "X", "date": {"days": ["T", "Th"], "start_time": "9:30AM", "end_time": "10:45AM"}, "name": 'Software Engineering', 'professor': 'X', 'description': 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.'},
+            {"code": "CSCI-UA.0002", "section": "X", "date": {"days": ["T", "Th"], "start_time": "9:30AM", "end_time": "10:45AM"}, "name": 'Software Engineering', 'professor': 'X', 'description': 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.'},
+            {"code": "CSCI-UA.0002", "section": "X", "date": {"days": ["T", "Th"], "start_time": "9:30AM", "end_time": "10:45AM"}, "name": 'Software Engineering', 'professor': 'X', 'description': 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.'},
+            {"code": "CSCI-UA.0002", "section": "X", "date": {"days": ["T", "Th"], "start_time": "9:30AM", "end_time": "10:45AM"}, "name": 'Software Engineering', 'professor': 'X', 'description': 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.'},
+            {"code": "CSCI-UA.0002", "section": "X", "date": {"days": ["T", "Th"], "start_time": "9:30AM", "end_time": "10:45AM"}, "name": 'Software Engineering', 'professor': 'X', 'description': 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.'},
+            {"code": "CSCI-UA.0002", "section": "X", "date": {"days": ["T", "Th"], "start_time": "9:30AM", "end_time": "10:45AM"}, "name": 'Software Engineering', 'professor': 'X', 'description': 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.'}
+        ]
+    return render_template('course_search.html', docs=docs)
+    
     
