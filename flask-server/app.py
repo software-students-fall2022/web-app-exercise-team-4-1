87577c916937b7d53f56b6da93d5b89d4abac052
Seok Tae Kim <stk319@nyu.edu>
2022-10-23 19:15:02 -0400

    remove section file

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from routes import views, course, section, authenticate
+from routes import views, course, authenticate
 from routes.users import admin, student
 
 app=Flask(__name__)
@@ -8,7 +8,6 @@ app.register_blueprint(views.app_blueprint)
 app.register_blueprint(course.course_blueprint)
 app.register_blueprint(student.student_blueprint)
 app.register_blueprint(admin.admin_blueprint)
-app.register_blueprint(section.section_blueprint)
 app.register_blueprint(authenticate.authenticate_blueprint)
 
 if __name__=='__main__':
